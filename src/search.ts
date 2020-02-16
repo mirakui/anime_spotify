34c7753fe709ad0651d8cfd21a4072789fc16bac
Issei Naruta <naruta@cookpad.com>
2020-02-16 22:23:19 +0900

    implement login

--- src/search.ts
+++ src/search.ts
@@ -1,28 +1,62 @@
 import axios, { AxiosResponse } from "axios";
-const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
+import querystring from "querystring";
 
-const ax = axios.create({
-  baseURL: "https://api.spotify.com",
-  headers: {
-    Authorization: "Bearer " + accessToken,
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
+const clientId = process.env.SPOTIFY_CLIENT_ID || "";
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || "";
 
-ax.get("/v1/search", {
-  params: {
-    q: "椎名林檎",
-    type: "track",
-    market: "JP"
+async function login(clientId: string, clientSecret: string) {
+  const ax = axios.create({
+    baseURL: "https://accounts.spotify.com",
+    headers: {
+      Authorization:
+        "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64")
+    }
+  });
+  try {
+    const params = {
+      grant_type: "client_credentials"
+    };
+    const res = await ax.post("/api/token", querystring.stringify(params), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+    const accessToken = res.data.access_token;
+    return accessToken;
+  } catch (err) {
+    console.log("login error:", err);
+    throw err;
   }
-})
-  .then((res: AxiosResponse) => {
-    console.log(res.data);
-    for (let item of res.data.tracks.items) {
-      console.log(item.name, item.artists[0].name);
+}
+
+async function main() {
+  const accessToken = await login(clientId, clientSecret);
+  console.log("accessToken=", accessToken);
+  const ax = axios.create({
+    baseURL: "https://api.spotify.com",
+    headers: {
+      Authorization: "Bearer " + accessToken,
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+
+  ax.get("/v1/search", {
+    params: {
+      q: "椎名林檎",
+      type: "track",
+      market: "JP"
     }
   })
-  .catch((err: any) => {
-    console.log(err);
-  });
+    .then((res: AxiosResponse) => {
+      console.log(res.data);
+      for (let item of res.data.tracks.items) {
+        console.log(item.name, item.artists[0].name);
+      }
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+}
+
+main();
